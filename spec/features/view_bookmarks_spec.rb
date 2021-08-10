26fc3ce7ae62d41915d636938d169f7bc22ac890
Dylan <Dylan@eequ.org>
2021-08-10 17:01:37 +0100

    refactored a lot of code and setup a test databse to run when running tests

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,16 @@
-feature "View Bookmarks" do
-  scenario "User wants to see all Bookmarks" do
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
-    save_and_open_page
-    expect(page).to have_content("http://www.twitter.com")
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
